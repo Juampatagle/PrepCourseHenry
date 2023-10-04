85a53e135336f3a756b0615bfc645a3eec8d95a5
Juampatagle <juampatagle@gmail.com>
2023-10-04 13:43:24 -0300

    agregue js

---
+++ operadores.js
@@ -0,0 +1,16 @@
+//suma
+
+console.log("Hola" + "Juan");
+var nombre = "Juan";
+var saludo = "Hola" + " " + nombre;
+
+console.log(saludo);
+
+//operadores de comparacion
+
+console.log (4 < 7); 
+console.log (4 < 1 );
+console.log (4 > 4); 
+console.log (4 == 7);
+
+// === igualdad estricta, verifica que coincidan el tipo de dato como el valor del mismo mientras que el == solo verifica que el valor sea el mismo
